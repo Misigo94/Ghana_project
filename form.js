e1b701f78e289b0882daaf7bf97b43145337c616
Misigomartin <Misigomartin@gmail.com>
2022-03-06 13:40:09 +0300

    add conditional statements to form.js

---
+++ form.js
@@ -0,0 +1,28 @@
+function userForm() {
+    let gender = document.getElementById("this_gender").value;
+    let bithday = document.getElementById("user_date").value;
+   if (this_gender == "") {
+       alert("You must choose a gender");
+   }
+   if(user_date == "") {
+       alert("You mus pick a date");
+   }
+}
+
+const dayArray = [
+    ["Sunday", "Kwasi", "Akosua"],
+    ["Monday", "Kwadwo", "Adwoa"],
+    ["Tuesday", "Kwabena", "Abenaa"],
+    ["Wednesday", "Kwaku", "Akua"],
+    ["Thursday", "Yaw", "Yaa"],
+    ["Friday", "Kofi", "Afua"],
+    ["Saturday", "Kwame", "Ama"],
+];
+
+// //conditional statements for error handling
+// if(this_gender == "") {
+//     alert("Please choose a gender");
+// }
+// else if ()
+    
+// }
